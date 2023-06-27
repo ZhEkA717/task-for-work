642dbc389084ae9ac88dcf2ece686d184dfd1bee
Zheka <grushevskiy.yevgeniy@mail.ru>
2023-06-27 12:49:36 +0300

    feat: add loading animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,27 @@
 const submit = document.querySelector('.submit-button');
+const progressBar = document.querySelector('.progress-bar');
 
 // animation for button
-const startAnimation = () => {
-    const isAnimation = event.currentTarget.classList.contains(('animate__animated','animate__shakeX'));
+const startAnimation = (event) => {
+    const isAnimation = event.target.classList.contains(('animate__animated','animate__shakeX'));
 
     if (!isAnimation) 
-        event.currentTarget.classList.add('animate__animated','animate__shakeX');
+        event.target.classList.add('animate__animated','animate__shakeX');
 }
 
 submit.addEventListener('click', (event) => {
     event.preventDefault();
 
-    startAnimation();
+    // startAnimation(event);
+
+    saveUser();
+
 });
 
 
 // end button animation
 submit.addEventListener('animationend', (event) => {
-    event.currentTarget.classList.remove('animate__animated','animate__shakeX');
+    event.target.classList.remove('animate__animated','animate__shakeX');
 })
 
 
@@ -29,3 +33,34 @@ formElements.forEach((item, i) => {
         item.classList.add('animate__animated', 'animate__fadeInUp');
     }, i * 250);
 })
+
+
+let isLoading = false;
+
+function saveUser() {
+    isWaitRequest();
+
+    fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: "post",
+        body: JSON.stringify({
+            completed:  false,
+            title: 'is okey',
+        })
+      }).then(res => {
+        isGetRequest();
+
+        if (res.status === 201) 
+            console.log('Thank you! You registered!')
+      })
+
+}
+
+function isWaitRequest() {
+    submit.disabled = true;
+    progressBar.style.display = 'block';
+}
+
+function isGetRequest() {
+    submit.disabled = false;
+    progressBar.style.display = 'none';
+}
